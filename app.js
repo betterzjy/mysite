bda6a89d04feee0f4ef353648e84f362b13cf658
zhaojinyang <zhaojinyang@meicai.cn>
2016-01-29 10:42:59 +0800

    add static

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 var express = require("express");
 var app = express();
 
+
+//中间件
+app.use(express.compress()); //压缩
+app.use(express.cookieParser()); //cookie支持
+app.use(express.cookieSession()); //session支持
+
+
 app.get("/", function (req, res) {
     res.send('hello world!');
+});
+app.get("/index", function(req, res) {
+    //res.json("h");
+    res.sendFile("static/index/html");
 });
 
 var server = app.listen(3000, function(){
